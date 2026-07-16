ae993b130c56af46e691727e253f1ee4ccd11d9d
Rahimull <rahimullahpashay@gmail.com>
2026-07-16 08:53:26 +0430

    work for deploy dmsApi on the render

--- DMSAPI/obj/Debug/net10.0/DMSAPI.AssemblyInfo.cs
+++ DMSAPI/obj/Debug/net10.0/DMSAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DMSAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+57d2e29276b80e7b7990147e2b92b43a3fb12535")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+399b20dd04fd3e839f0ee6d5295975d6d050cdc0")]
 [assembly: System.Reflection.AssemblyProductAttribute("DMSAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DMSAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
